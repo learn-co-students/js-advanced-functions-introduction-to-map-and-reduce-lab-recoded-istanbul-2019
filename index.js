59c4b4196c2e7530964f21240d9c08cac8d99df8
Oktay Ibis <oktayibis@gmail.com>
2020-02-07 22:30:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(v => {
+        return v * -1;
+    })
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(v => {
+        return v;
+    })
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map( v => {
+      return  v * 2;
+    })
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(v => {
+        return Math.pow(v,2);
+    })
+}
+
+//reduce metods
+
+function reduceToTotal(sourceArray, start=0){
+    return sourceArray.reduce(function(memo, v) {
+        return memo += v;
+    }, start)
+}
+
+function reduceToAllTrue(sourceArray, start=true){
+    return sourceArray.reduce(function(memo, v){
+        if(v && memo){
+            memo = true
+            return memo
+        } else {
+            memo =false
+            return memo
+        }
+    }, start)
+}
+
+function reduceToAnyTrue(sourceArray, start=false){
+    return sourceArray.reduce(function(memo, v){
+        if(v){
+            memo =true;
+        }
+        return memo;
+
+    }, start)
+}
