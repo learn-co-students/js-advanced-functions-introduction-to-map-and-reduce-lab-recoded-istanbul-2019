262e22270afd6bb74dcca7861a6d84045b3da49e
mira_arar <mira.ar230@gmail.com>
2020-05-01 19:00:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+/////////////////map-like////////////////////////
+const mapToNegativize=(sourceArray)=>{
+    let resaultArray=[];
+    sourceArray.forEach(element =>resaultArray.push(element*-1))
+    return resaultArray;    
+}
+const mapToNoChange=(sourceArray)=>{return [...sourceArray]}
+const mapToDouble =(sourceArray) =>{
+    let doubledArray=[];
+    sourceArray.forEach(element =>doubledArray.push(element*2))
+    return doubledArray;
+}
+const mapToSquare=(sourceArray)=>{
+    let squared=[];
+    sourceArray.forEach(element =>squared.push(element*element))
+    return squared;
+}
+/////////////////reduce-like////////////////////////
+const reduceToTotal=(sourceArray, startingPoint=0)=>{
+    sourceArray.forEach(element =>startingPoint+=element)
+    return startingPoint;
+}
+const reduceToAllTrue=(sourceArray)=>{return sourceArray.every(element =>Boolean(element)=== true)};
+const reduceToAnyTrue=(sourceArray)=>{return sourceArray.some(element =>Boolean(element)=== true)};
+
+
+
+
