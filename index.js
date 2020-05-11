d78df6875588f3641da133eaa81d36a58aa77a1a
samah_mustafa <i.samaaah@gmail.com>
2020-05-12 01:10:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(function(x){
+        return x * -1;
+    })
+};
+
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(function(x){
+        return x;
+    })
+}
+
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(function(x){
+        return x * 2;
+    })
+}
+
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(function(x){
+        return x * x;
+    })
+}
+
+
+///////
+
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    return sourceArray.reduce(function(num, value) {
+        return num + value
+    }, startingPoint)
+}
+
+
+function reduceToAllTrue(sourceArray) {
+    return sourceArray.reduce(function(num, value) {
+        if (value) {
+            return true;
+        } else{
+            return false;
+        }
+    })
+}
+
+
+function reduceToAnyTrue(sourceArray) {
+    return sourceArray.reduce(function(num, value) {
+        if (value) {
+            return true;
+        } else{
+            return false;
+        }
+    })
+}
